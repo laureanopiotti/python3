d3f394b01afc5d172d492b600b5a75f14f3bdee6
Laureano Piotti <laureanopiotti@gmail.com>
2019-03-06 20:26:02 -0300

    class and static method

--- 04_Object_Oriented_Python/016_more_staticmethod_classmethod.py
+++ 04_Object_Oriented_Python/016_more_staticmethod_classmethod.py
@@ -20,19 +20,19 @@ We have this `FixedFloat` class that is really basic—doesn’t really do anyth
 Imagine we wanted to get a new `FixedFloat` object which is a result of summing two numbers together:
 """
 
-class FixedFloat:
-    def __init__(self, amount):
-        self.amount = amount
+# class FixedFloat:
+#     def __init__(self, amount):
+#         self.amount = amount
 
-    def __repr__(self):
-        return f'<FixedFloat {self.amount:.2f}>'
+#     def __repr__(self):
+#         return f'<FixedFloat {self.amount:.2f}>'
 
-    def from_sum(self, value1, value2):
-        return FixedFloat(value1 + value2)
+#     def from_sum(self, value1, value2):
+#         return FixedFloat(value1 + value2)
 
-number = FixedFloat(18.5746)
-new_number = number.from_sum(19.575, 0.789)
-print(new_number)
+# number = FixedFloat(18.5746)
+# new_number = number.from_sum(19.575, 0.789)
+# print(new_number)
 
 """
 This doesn’t make any sense, because we created a `FixedFloat` object (`number`), and then proceeded to call an instance method to create a new object. But that instance method didn’t use `self` at all—so really the fact that it’s a method inside a class is not very useful.
@@ -40,19 +40,19 @@ This doesn’t make any sense, because we created a `FixedFloat` object (`number
 Instead, we could make it a `@staticmethod`. That way, we’re not getting `self` but we can still put the method in the class, since it is _related_ to the class:
 """
 
-class FixedFloat:
-    def __init__(self, amount):
-        self.amount = amount
+# class FixedFloat:
+#     def __init__(self, amount):
+#         self.amount = amount
 
-    def __repr__(self):
-        return f'<FixedFloat {self.amount:.2f}>'
+#     def __repr__(self):
+#         return f'<FixedFloat {self.amount:.2f}>'
 
-    @staticmethod
-    def from_sum(value1, value2):
-        return FixedFloat(value1 + value2)
+#     @staticmethod
+#     def from_sum(value1, value2):
+#         return FixedFloat(value1 + value2)
 
-static_number = FixedFloat.from_sum(19.575, 0.789)
-print(static_number)
+# static_number = FixedFloat.from_sum(19.575, 0.789)
+# print(static_number)
 
 """
 That looks a bit better! Now we don’t have the useless parameter AND we don’t need to create an object before we can call the method. Win-win!
@@ -74,11 +74,11 @@ class Euro(FixedFloat):
 We’ve defined this new class that extends the `FixedFloat ` class. It’s got an `__init__` method that calls the parent’s `__init__`, and a `__repr__` method that overrides the parents’. It doesn’t have a `from_sum` method as that’s inherited and we’ll just use the one the parent defined.
 """
 
-euros = Euro(18.5963)
-print(euros)  # <Euro €18.59>
+# euros = Euro(18.5963)
+# print(euros)  # <Euro €18.59>
 
-result = Euro.from_sum(15.76, 19.905)
-print(result)  # <FixedFloat 35.66>
+# result = Euro.from_sum(15.76, 19.905)
+# print(result)  # <FixedFloat 35.66>
 
 """
 Oops! When we called the `Euro` constructor directly, we got a `Euro ` object with the symbol. But when we call `from_sum`, we got a `FixedFloat ` object. Not what we wanted!
@@ -91,25 +91,25 @@ In order to fix this, we must make the `from_sum` method return an object of the
 `@classmethod` to the rescue! If we modify the `FixedFloat` class:
 """
 
-class FixedFloat:
-    def __init__(self, amount):
-        self.amount = amount
+# class FixedFloat:
+#     def __init__(self, amount):
+#         self.amount = amount
 
-    def __repr__(self):
-        return f'<FixedFloat {self.amount:.2f}>'
+#     def __repr__(self):
+#         return f'<FixedFloat {self.amount:.2f}>'
 
-    @classmethod
-    def from_sum(cls, value1, value2):
-        return cls(value1 + value2)
+#     @classmethod
+#     def from_sum(cls, value1, value2):
+#         return cls(value1 + value2)
 
 
-class Euro(FixedFloat):
-    def __init__(self, amount):
-        super().__init__(amount)
-        self.symbol = '€'
+# class Euro(FixedFloat):
+#     def __init__(self, amount):
+#         super().__init__(amount)
+#         self.symbol = '€'
 
-    def __repr__(self):
-        return f'<Euro {self.symbol}{self.amount:.2f}>'
+#     def __repr__(self):
+#         return f'<Euro {self.symbol}{self.amount:.2f}>'
 
 """
 When we now call:
@@ -118,4 +118,4 @@ When we now call:
 * `FixedFloat.from_sum()`, `cls` is the `FixedFloat` class.
 """
 
-print(Euro.from_sum(16.7565, 90))  # <Euro €106.75>
+# print(Euro.from_sum(16.7565, 90))  # <Euro €106.75>
