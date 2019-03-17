77cf28a7182761642c667cf0b3069daaa12d5f4c
Laureano Piotti <laureanopiotti@gmail.com>
2019-03-17 13:29:21 -0300

    web scaping project 1

--- 10_Web_Scraping/1004_class_html_parsing.py
+++ 10_Web_Scraping/1004_class_html_parsing.py
@@ -33,17 +33,39 @@ ITEM_HTML = '''<html><head></head><body>
 '''
 
 
-soup = BeautifulSoup(ITEM_HTML, 'html.parser')
+class ParsedItem:
+    """
+    A class to take in an HTML page or content, and find properties of an item
+    in it.
+    """
+    def __init__(self, page):
+        self.soup = BeautifulSoup(page, 'html.parser')
 
-def find_item_price():
-    locator = 'article.product_pod p.price_color'
-    item_price = soup.select_one(locator).string
-    
-    pattern = '£([0-9]+\.[0-9]+)'
-    matcher = re.search(pattern,item_price)
+    def name(self):
+        locator = 'article.product_pod h3 a'
+        item_name = self.soup.select_one(locator).attrs['title']
+        return item_name
 
-    print(matcher.group(0))
-    print(matcher.group(1))
+    def link(self):
+        locator = 'article.product_pod h3 a'
+        item_url = self.soup.select_one(locator).attrs['href']
+        return item_url
 
-find_item_price()
+    def price(self):
+        locator = 'article.product_pod p.price_color'
+        item_price = self.soup.select_one(locator).string
 
+        pattern = '£([0-9]+\.[0-9]+)'
+        matcher = re.search(pattern, item_price)
+        return float(matcher.group(1))
+
+    def rating(self):
+        locator = 'article.product_pod p.star-rating'
+        star_rating_element = self.soup.select_one(locator)
+        classes = star_rating_element.attrs['class']
+        rating_classes = filter(lambda x: x != 'star-rating', classes)
+        return next(rating_classes)
+
+
+item = ParsedItem(ITEM_HTML)
+print(item.price())
---
+++ 10_Web_Scraping/Milestone_Project_03/01_scraping_quotes/locators/quote_locators.py
@@ -0,0 +1,4 @@
+class QuoteLocators:
+    AUTHOR = 'small.author'
+    CONTENT = 'span.text'
+    TAGS = 'div.tag a.tag'
