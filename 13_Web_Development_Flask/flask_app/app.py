0dee05356e3f5e6c4134a2789f18c70b060c68af
Laureano Piotti <laureanopiotti@gmail.com>
2019-03-21 00:39:18 -0300

    flask

---
+++ 13_Web_Development_Flask/flask_app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+posts = {
+    0: {
+        'title': 'Hello, world',
+        'content': 'This is my first blog post!'
+    }
+}
+
+@app.route('/')
+def home():
+    return 'Hello world !'
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        return render_template('404.html', message=f'A post with id {post_id} was not found.')
+    return render_template('post.html', post=post)
+
+if __name__ == '__main__':
+    app.run(debug=True)
